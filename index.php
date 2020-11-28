d37d31e9c6f869ac70e2532b83a6d84d81e4b907
elvinrsb91 <75135625+elvinrsb91@users.noreply.github.com>
2020-11-28 07:35:04 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,101 +1,147 @@
-
-
-<html lang="en">
-
+<!DOCTYPE html>
+<html class="html" lang="es-ES">
  <head>
 
-</head>
+  <script type="text/javascript">
+   if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "webpro.js", "musewpslideshow.js", "jquery.museoverlay.js", "touchswipe.js", "jquery.watch.js", "index.css"], "outOfDate":[]};
+</script>
+  
+  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+  <meta name="generator" content="2015.0.1.310"/>
+  <title>Home</title>
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="css/site_global.css?4052507572"/>
+  <link rel="stylesheet" type="text/css" href="css/index.css?3867726017" id="pagesheet"/>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   document.documentElement.className += ' js';
+</script>
+   </head>
+ <body>
 
-<head>
-	    
-<title>TUYO</title>
-	
-<link rel="stylesheet" href="CSSB/bootstrap.min.css">
-<link rel="icon" type="img/png" href="img/icono-01.png">
-<script src="js/jquery_3.4.1.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>	
-<link rel="stylesheet" type="text/css" href="cssaler/alertify.css">
-<link rel="stylesheet" type="text/css" href="cssaler/themes/default.css">
-<script src="alert/alertify.js"></script>
-<script src="js/typeahead.min.js"></script>
-
-
-<!------------------------------------------------------------------------------------------->	
-	
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta name="viewport" content="width=device-width,user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
-</head>
-
-<!------------------------------------------------------------------------------------------->
-	<body >
-
-<nav class="navbar navbar-expand-lg navbar-light bg-info">
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon">
-	  </span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href=""></a>
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+  <div class="clearfix" id="page"><!-- column -->
+   <div class="position_content" id="page_position_content">
+    <div class="clearfix colelem" id="pu204"><!-- group -->
+     <div class="clip_frame grpelem" id="u204"><!-- image -->
+      <img class="block" id="u204_img" src="images/fondo-02-01.png" alt="" width="1147" height="1384"/>
+     </div>
+     <div class="clip_frame grpelem" id="u81"><!-- image -->
+      <img class="block" id="u81_img" src="images/logo-01.png" alt="" width="200" height="195"/>
+     </div>
+     <img class="grpelem" id="u87-4" alt="Te damos la bienvenida a tu red social" width="309" height="137" src="images/u87-4.png"/><!-- rasterized frame -->
+     <div class="SlideShowWidget clearfix grpelem" id="slideshowu96"><!-- none box -->
+      <div class="popup_anchor" id="u105popup">
+       <div class="SlideShowContentPanel clearfix" id="u105"><!-- stack box -->
+        <div class="SSSlide clip_frame clearfix grpelem" id="u110"><!-- image -->
+         <div id="u110_clip">
+          <img class="ImageInclude position_content" id="u110_img" data-src="images/baners-01.png" src="images/blank.gif" alt="" data-width="637" data-height="788"/>
+         </div>
+        </div>
+        <div class="SSSlide invi clip_frame clearfix grpelem" id="u155"><!-- image -->
+         <div id="u155_clip">
+          <img class="ImageInclude position_content" id="u155_img" data-src="images/baners-02.png" src="images/blank.gif" alt="" data-width="637" data-height="788"/>
+         </div>
+        </div>
+        <div class="SSSlide invi clip_frame clearfix grpelem" id="u164"><!-- image -->
+         <div id="u164_clip">
+          <img class="ImageInclude position_content" id="u164_img" data-src="images/baners-03.png" src="images/blank.gif" alt="" data-width="637" data-height="788"/>
+         </div>
+        </div>
+       </div>
+      </div>
+      <div class="popup_anchor" id="u119-4popup">
+       <div class="SlideShowLabel SSSlideCount clearfix" id="u119-4"><!-- content -->
+        <p><span id="u119">3 - 3</span></p>
+       </div>
+      </div>
+      <div class="popup_anchor" id="u120-4popup">
+       <div class="SSPreviousButton clearfix" id="u120-4"><!-- content -->
+        <p><span id="u120">&lt;</span></p>
+       </div>
+      </div>
+      <div class="popup_anchor" id="u104-4popup">
+       <div class="SSNextButton clearfix" id="u104-4"><!-- content -->
+        <p>&gt;</p>
+       </div>
+      </div>
+     </div>
+     <img class="grpelem" id="u175-4" alt="Únete hoy mismo" width="371" height="77" src="images/u175-4.png"/><!-- rasterized frame -->
+     <div class="clearfix grpelem" id="u180"><!-- group -->
+      <div class="clip_frame grpelem" id="u187"><!-- image -->
+       <img class="block" id="u187_img" src="images/iconos%20de%20web-02.png" alt="" width="51" height="51"/>
+      </div>
+     </div>
+     <div class="clip_frame grpelem" id="u196"><!-- image -->
+      <img class="block" id="u196_img" src="images/tuyo%20en%20blanco-03.png" alt="" width="209" height="87"/>
+     </div>
+     <div class="clearfix grpelem" id="u202-9"><!-- content -->
+      <p id="u202-2">Red social dirigida a los adultos mayores que les permitirá compartir contenido, emociones, que el usuario gane confianza y empatía por medio de fotografía, videos y audios. Una parte novedosa de la red social permitirá el intercambio de conocimientos con personas de distintos países, organizaciones religiosas, civiles y sociales.</p>
+      <p id="u202-3">&nbsp;</p>
+      <p id="u202-5"><span id="u202-4">Con este servicio se pretende conectar a los adultos mayores con otras personas desde una cuenta personal de manera virtual a nivel global.</span></p>
+      <p id="u202-6">&nbsp;</p>
+      <p>&nbsp;</p>
+     </div>
+     <div class="clip_frame grpelem" id="u210"><!-- image -->
+      <img class="block" id="u210_img" src="images/logotuyo.png" alt="" width="153" height="150"/>
+     </div>
+     <img class="grpelem" id="u219-4" alt="Centro de Ayuda" width="199" height="32" src="images/u219-4.png"/><!-- rasterized frame -->
+     <img class="grpelem" id="u227-4" alt="Aplican condiciones" width="224" height="32" src="images/u227-4.png"/><!-- rasterized frame -->
+     <img class="grpelem" id="u228-4" alt="Políticas de condiciones" width="250" height="32" src="images/u228-4.png"/><!-- rasterized frame -->
+     <img class="grpelem" id="u229-4" alt="Guía de configuración" width="250" height="32" src="images/u229-4.png"/><!-- rasterized frame -->
 		
-	<li class="nav-item">	
-    
-	</li>
-		<li class="nav-item">
-      <a class="nav-link" href="">
-		  <img class="img-fluid text-center" src="img/HOME.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span></a>
-      </li>
+     <div class="Button rounded-corners clearfix grpelem" id="buttonu220"><!-- container box -->
+      <a href=""><img class="grpelem" id="u221" alt="Registrarse" src="images/blank.gif"/></a><!-- state-based BG images -->
+     </div>
 		
-		<li class="nav-item">
-      <a class="nav-link" href="">
-		  <img class="img-fluid text-center" src="img/tuyo-01.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span></a>
-      </li>
+     <div class="Button rounded-corners clearfix grpelem" id="buttonu223"><!-- container box -->
+      <a href="redsocial/index.php"><img class="grpelem" id="u224" alt="Iniciar sesión" src="images/blank.gif"/></a><!-- state-based BG images -->
+     </div>
 		
-		
-		<li class="nav-item">
-      <a class="nav-link" href="buscador.php">
-		  <img class="img-fluid text-center" src="img/tuyo-03.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span></a>
-      </li>
-		
-		<li class="nav-item">
-      <a class="nav-link" href="Form_Abono.php">
-		  <img class="img-fluid text-center" src="img/tuyo-05.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span></a>
-      </li>
-		<li class="nav-item">
-      <a class="nav-link" href="Form_Abono.php">
-		  <img class="img-fluid text-center" src="img/tuyo-04.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span></a>
-      </li>
-		
-		
-			<li class="nav-item">
-      <a class="nav-link" href="../close.php">
-		  <img class="img-fluid text-center" src="img/tuyo-06.png" width="120" height="120" alt="Card image cap">
-		  <span class="font-weight-bold"></span>
-			</a>
-      </li>
-    </ul>
-   
-	  
-	   <ul class="navbar-nav px-3">       
-        <li class="nav-item text-nowrap">
-	  <nav class="navbar navbar-expand-lg navbar-light bg-info">
-	  <a class="navbar-brand">		   
-	     <img src="" width="24" height="24" alt="">
-	     
-		  </a> 		
-	 
-	</nav>
-	</li>
-       </ul>
+     <div class="clip_frame grpelem" id="u230"><!-- image -->
+      <img class="block" id="u230_img" src="images/logo-02.png" alt="" width="157" height="153"/>
+     </div>
+    </div>
+    <div class="verticalspacer"></div>
+    <div class="clip_frame colelem" id="u181"><!-- image -->
+     <img class="block" id="u181_img" src="images/iconos%20de%20web-01.png" alt="" width="57" height="57"/>
+    </div>
+    <div class="browser_width colelem" id="u173-bw">
+     <div id="u173"><!-- simple frame --></div>
+    </div>
+   </div>
   </div>
-</nav>
-
-<script src="js/bootstrap.min.js"></script>
- </body>	
+  <div class="preload_images">
+   <img class="preload" src="images/u221-r.png" alt=""/>
+   <img class="preload" src="images/u224-r.png" alt=""/>
+  </div>
+  <!-- JS includes -->
+  <script type="text/javascript">
+   if (document.location.protocol != 'https:') document.write('\x3Cscript src="http://musecdn.businesscatalyst.com/scripts/4.0/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script type="text/javascript">
+   window.jQuery || document.write('\x3Cscript src="scripts/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
+</script>
+  <script src="scripts/museutils.js?334180058" type="text/javascript"></script>
+  <script src="scripts/webpro.js?7309900" type="text/javascript"></script>
+  <script src="scripts/musewpslideshow.js?4149944274" type="text/javascript"></script>
+  <script src="scripts/jquery.museoverlay.js?4282111687" type="text/javascript"></script>
+  <script src="scripts/touchswipe.js?526709374" type="text/javascript"></script>
+  <script src="scripts/jquery.watch.js?293013060" type="text/javascript"></script>
+  <!-- Other scripts -->
+  <script type="text/javascript">
+   $(document).ready(function() { try {
+(function(){var a={},b=function(a){if(a.match(/^rgb/))return a=a.replace(/\s+/g,"").match(/([\d\,]+)/gi)[0].split(","),(parseInt(a[0])<<16)+(parseInt(a[1])<<8)+parseInt(a[2]);if(a.match(/^\#/))return parseInt(a.substr(1),16);return 0};(function(){$('link[type="text/css"]').each(function(){var b=($(this).attr("href")||"").match(/\/?css\/([\w\-]+\.css)\?(\d+)/);b&&b[1]&&b[2]&&(a[b[1]]=b[2])})})();(function(){$("body").append('<div class="version" style="display:none; width:1px; height:1px;"></div>');
+for(var c=$(".version"),d=0;d<Muse.assets.required.length;){var f=Muse.assets.required[d],g=f.match(/([\w\-\.]+)\.(\w+)$/),k=g&&g[1]?g[1]:null,g=g&&g[2]?g[2]:null;switch(g.toLowerCase()){case "css":k=k.replace(/\W/gi,"_").replace(/^([^a-z])/gi,"_$1");c.addClass(k);var g=b(c.css("color")),h=b(c.css("background-color"));g!=0||h!=0?(Muse.assets.required.splice(d,1),"undefined"!=typeof a[f]&&(g!=a[f]>>>24||h!=(a[f]&16777215))&&Muse.assets.outOfDate.push(f)):d++;c.removeClass(k);break;case "js":k.match(/^jquery-[\d\.]+/gi)&&
+typeof $!="undefined"?Muse.assets.required.splice(d,1):d++;break;default:throw Error("Unsupported file type: "+g);}}c.remove();if(Muse.assets.outOfDate.length||Muse.assets.required.length)c="Some files on the server may be missing or incorrect. Clear browser cache and try again. If the problem persists please contact website author.",(d=location&&location.search&&location.search.match&&location.search.match(/muse_debug/gi))&&Muse.assets.outOfDate.length&&(c+="\nOut of date: "+Muse.assets.outOfDate.join(",")),d&&Muse.assets.required.length&&(c+="\nMissing: "+Muse.assets.required.join(",")),alert(c)})()})();
+/* body */
+Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
+Muse.Utils.prepHyperlinks(true);/* body */
+Muse.Utils.initWidget('#slideshowu96', function(elem) { $(elem).data('widget', new WebPro.Widget.ContentSlideShow(elem, {autoPlay:true,displayInterval:3500,slideLinkStopsSlideShow:false,transitionStyle:'fading',lightboxEnabled_runtime:false,shuffle:false,transitionDuration:500,enableSwipe:true,elastic:'off',resumeAutoplay:true,resumeAutoplayInterval:3000,playOnce:false,autoActivate_runtime:false})); });/* #slideshowu96 */
+Muse.Utils.resizeHeight()/* resize height */
+Muse.Utils.fullPage('#page');/* 100% height page */
+Muse.Utils.showWidgetsWhenReady();/* body */
+Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
+} catch(e) { if (e && 'function' == typeof e.notify) e.notify(); else Muse.Assert.fail('Error calling selector function:' + e); }});
+</script>
+   </body>
 </html>
